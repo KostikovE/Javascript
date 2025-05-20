8084e3e8bec4c8c5a601e912ddfeb143a4e1541d
KostikovE <uitro355@gmail.com>
2025-05-20 14:43:57 +0300

    Update lab2.js

--- lab2/lab2.js
+++ lab2/lab2.js
@@ -1,1 +1,16 @@
-
+/**
+ * Возвращает x в степени n (n - целое число).
+ * @param {number} x - Основание степени.
+ * @param {number} n - Показатель степени (целое число).
+ * @returns {number} Результат возведения x в степень n.
+ */
+function pow(x, n) {
+    if (n === 0) return 1;
+    if (n < 0) return 1 / pow(x, -n);
+    
+    let result = 1;
+    for (let i = 0; i < n; i++) {
+        result *= x;
+    }
+    return result;
+}
