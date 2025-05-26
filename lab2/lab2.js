0a3fd317463d08ae64787bffdf15251ab2886bd6
KostikovE <uitro355@gmail.com>
2025-05-26 16:59:28 +0300

    Update lab2.js

--- lab2/lab2.js
+++ lab2/lab2.js
@@ -42,3 +42,21 @@ function factorial(n) {
     if (n === 0) return 1n;
     return BigInt(n) * factorial(n - 1);
 }
+
+/**
+ * Возвращает n-е число Фибоначчи.
+ * @param {number} n - Порядковый номер числа Фибоначчи.
+ * @returns {bigint} n-е число Фибоначчи в виде BigInt.
+ */
+function fib(n) {
+    let a = 0n;
+    let b = 1n;
+    if (n === 0) return a;
+    
+    for (let i = 2; i <= n; i++) {
+        const c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
