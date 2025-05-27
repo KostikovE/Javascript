5a90368aca7a421d17b6b1a5e650f0f98c241acf
KostikovE <uitro355@gmail.com>
2025-05-27 10:54:38 +0300

    Update lab3.js

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -1,1 +1,14 @@
+'use strict';
 
+import{
+  fib
+} from './fib.js';
+
+/**
+* Возвращает дробную часть числа.
+* @param {number} num - число.
+* @returns {number} Дробная часть числа.
+*/
+export function getDecimal(num){
+  return +(num >=0 ? num % 1 : 1 + (num % 1)).toFixed(2);
+}
