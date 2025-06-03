98583ee30dc9a2e148ebca9d8c86fc52a97a461b
KostikovE <uitro355@gmail.com>
2025-06-03 11:18:03 +0300

    Update lab4.js

--- lab4/lab4.js
+++ lab4/lab4.js
@@ -140,7 +140,7 @@ try {
         const day = String(date.getDate()).padStart(2,'0');
         const month = String(date.getMonth() + 1).padStart(2,'0');
         const year = date.getFullYear();
-        return `${day}.${month}.${year};
+        return `${day}.${month}.${year}`;
     }
 
     let date1 = new Date(2024, 0, 20); 
