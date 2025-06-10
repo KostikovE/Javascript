281b11cec5f7a1f82c2c8c31e68a400883f11070
KostikovE <uitro355@gmail.com>
2025-06-10 09:44:08 +0300

    Update lab4.js

--- lab4/lab4.js
+++ lab4/lab4.js
@@ -113,7 +113,7 @@ try {
     }
 
     let obj1 = { [Symbol()]: true };
-    let obj2 = {};
+    let obj2 = Object.defineProperty({}, 'name', { value: 'John', });
 
     console.log("Объект 1", isEmpty(obj1));
     console.log("Объект 2", isEmpty(obj2));
